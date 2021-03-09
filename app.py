b39864d6119fe4e7f4d1fc354083a3c9a39c0899
Mason Edwards <edwardsmason2000@gmail.com>
2021-03-09 02:50:32 +0000

    Added Database link, and topics from database are shown on homepage

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template, url_for, request, redirect
+import mysql.connector
+
+conn = mysql.connector.connect(
+    user="root", password="root", host="127.0.0.1", database="forum")
+cursor = conn.cursor()
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    cursor.execute(
+        "SELECT topic.topic, user.username from topic join user on topic.createdBy=user.userID;")
+    topicData = cursor.fetchall()
+
+    for(topic) in topicData:
+        print(topic)
+    return render_template('index.html', topicData=topicData)
 
 
 if (__name__) == "__main__":
